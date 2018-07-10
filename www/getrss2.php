8bdec3c7b421ddb10156025e12b16bfce1653c18
tshr20171114 <33654977+tshr20171114@users.noreply.github.com>
2018-07-11 05:28:16 +0900

    Update getrss2.php

--- www/getrss2.php
+++ www/getrss2.php
@@ -128,7 +128,8 @@ __HEREDOC__;
     }
     $href = $matches1[1];
     $thumbnail = 'https:' . $matches2[1];
-    $title = $matches3[1] . ' ' . htmlspecialchars($matches4[1]);
+    // $title = $matches3[1] . ' ' . htmlspecialchars($matches4[1]);
+    $title = $matches3[1] . ' ' . $matches4[1];
    
     $statement_select->execute(
       [
