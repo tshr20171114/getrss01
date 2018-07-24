c9a89f324c5dae9964c0c0b4fbbe30106b01c66d
tshr20171114 <33654977+tshr20171114@users.noreply.github.com>
2018-07-25 04:05:21 +0900

    Update getlink.php

--- www/getlink.php
+++ www/getlink.php
@@ -1,7 +1,38 @@
 <?php
 
+$pid = getmypid();
+
 $url = $_GET['u'];
 
+error_log("${pid} ${url}");
 
+$res = file_get_contents($url);
+
+$rc = preg_match('/<div class="gotoBlog"><a href="(.+?)"/', $res, $matches);
+
+if ($rc != 1) {
+  exit;
+}
+
+$url2 = $matches[1];
+
+error_log("${pid} ${url2}");
+
+$res = file_get_contents($url2);
+
+$pattern1 = getenv('PATTERN1');
+
+$rc = preg_match('/' . $pattern1 . '/', $res, $matches);
+
+if ($rc != 1) {
+  exit;
+}
+
+$url3 = $matches[1];
+
+error_log("${pid} ${url3}");
+
+header('Content-Type: text/plain');
+echo  $url3;
 
 ?>
