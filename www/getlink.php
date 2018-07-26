1df60bf0aef161c54abcb6770d66f311646819ca
tshr20171114 <33654977+tshr20171114@users.noreply.github.com>
2018-07-27 03:24:46 +0900

    Update getlink.php

--- www/getlink.php
+++ www/getlink.php
@@ -20,15 +20,21 @@ error_log("${pid} ${url2}");
 
 $res = file_get_contents($url2);
 
+/*
 $pattern1 = getenv('PATTERN1');
 
 $rc = preg_match('/' . $pattern1 . '/', $res, $matches);
+*/
+
+$pattern2 = getenv('PATTERN2');
+$rc = preg_match('/' . $pattern2 . '/', $res, $matches);
 
 if ($rc != 1) {
   exit;
 }
 
 $url3 = $matches[1];
+
 
 error_log("${pid} ${url3}");
 
