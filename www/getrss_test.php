d263011754e82814636fb98fcec6859f0d1508b0
tshr20171114 <33654977+tshr20171114@users.noreply.github.com>
2018-07-27 04:10:40 +0900

    Update getrss_test.php

--- www/getrss_test.php
+++ www/getrss_test.php
@@ -119,6 +119,8 @@ __HEREDOC__;
       $thumbnail_hash = $result['thumbnail_hash'];
     }
     
+    $rc = file_get_contents('https://' . getenv('SELF_FQDN') '/getlink.php?u=' . $href);
+    error_log("${pid} ${rc}");
     //error_log("${time} ${title} ${href} ${thumbnail} ${page_}");
     error_log("${pid} ${href} ${title}");
    
